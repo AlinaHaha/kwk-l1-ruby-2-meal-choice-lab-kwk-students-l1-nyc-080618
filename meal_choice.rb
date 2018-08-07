3a98e86aac88160c0280f9ee90680e808fc20593
AlinaHaha <2ndalina@gmail.com>
2018-08-07 15:14:44 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,20 +2,10 @@ def breakfast(food="frosted flakes")
   "Morning is for cereal like #{food}!"
 end
 
+puts breakfast()
 
 
 
 
 
-
-
-
-
-puts snacks("Swedish fish")
-## This returns "Any time, is the right time for Sweedish fish!"
-
-# call your methods here
-
-
-# Call your methods without any arguments here
 
